4b196cfd3705bc8f9bc4223dac6433696fb37cf3
Sufian_Majid <sufian.majid.999@gmail.com>
2024-01-28 23:24:07 +0500

    Basic queries and soft delete implementation for delete only. Also findMany returns only not deleted.

--- index.js
+++ index.js
@@ -1,45 +1,125 @@
 const { PrismaClient } = require('@prisma/client');
+const { PrismaClientKnownRequestError } = require('@prisma/client/runtime/library');
+const { randomData } = require('./util');
 
-const prisma = new PrismaClient();
-
-main = {
-	//Create Driver
-	createDriver: async () => {
-		try {
-			return await prisma.driver.create({
-				data: {
-					email: 'something01@gmai.com',
-					name: 'radiohead',
-				},
-			});
-		} catch (e) {
-			console.error(e);
-			throw Error(e);
-		}
+const prisma = new PrismaClient({ log: ['query', 'info', 'warn', 'error'] }).$extends({
+	name: 'soft-delete',
+	query: {
+		$allModels: {
+			async findMany({ args, query }) {
+				args.where = { ...args, deleted: false };
+				return query(args);
+			},
+			async findUniqueOrThrow({ args, query }) {
+				args.where = { ...args, deleted: false };
+			},
+			async delete({ model, args }) {
+				return prisma[model].update({
+					...args,
+					data: { deleted: true },
+				});
+			},
+		},
 	},
+});
 
-	// Get driver
-	getDriver: async (id) => {
-		try {
-			return await prisma.driver.findUniqueOrThrow({ where: { id: id } });
-		} catch (e) {
-			if (e.code === 'P2025') {
-				console.log('Driver not found');
-			} else {
-				console.error(e);
-				throw Error(e);
-			}
+const main = {
+	//POST one
+	createDriver: async (data) => {
+		return await prisma.driver.create({ data: randomData(['name', 'email']) });
+	},
+	// POST many
+	createManyDrivers: async (length) => {
+		let arr = [];
+		for (let i = 0; i <= length; i++) {
+			arr.push(randomData(['name', 'email']));
 		}
+		return await prisma.driver.createMany({
+			data: arr,
+			skipDuplicates: true,
+		});
+	},
+	// GET one
+	getDriver: async (id) => {
+		return await prisma.driver.findUniqueOrThrow({ where: { id: id } });
+	},
+	// GET all
+	getAllDrivers: async () => {
+		return await prisma.driver.findMany();
+	},
+	// PATCH one
+	updateDriver: async (id) => {
+		return await prisma.driver.update({
+			where: { id: id },
+			data: randomData(['name']),
+		});
+	},
+	//PATCH many
+	updateManyDrivers: async (ids) => {
+		return await prisma.driver.updateMany({
+			where: { id: { in: ids } },
+			data: randomData(['name']),
+		});
+	},
+	//DELETE one
+	deleteDriver: async (id) => {
+		return await prisma.driver.delete({ where: { id: id } });
+	},
+	//UPSERT or FINDSERT [ Update ? update:create OR if update:"empty" then find ? find:create ]
+	upsertDriver: async (name) => {
+		return await prisma.driver.upsert({
+			where: { name: name },
+			update: randomData(['email']), //OR update:''
+			create: randomData(['name', 'email']),
+		});
 	},
 };
-main.createDriver()
+/*
+                          ------------Driver-------------
+    createDriver();
+    createManyDrivers(length)
+    getAllDrivers()
+    getDriver(id)
+    updateDriver(id)
+    updateManyDrivers(Array[ids])
+    deleteDriver(id)
+    upsertDriver(name)
+                           ------------Cars---------------
+                           They are all same bro, except it has driver_id.
+                           plus, relations in ORM's are more dependent on schema.
+
+     Transaction:
+     var postsCreated = await prisma.$transaction(posts)
+
+
+     soft-delete:
+     We can use prisma extensions or create our own
+
+
+    */
+
+main.getAllDrivers()
 	.then((data) => {
 		console.log('Ran successfully!');
 		console.log(data);
 	})
 	.catch((e) => {
-		console.error(e);
+		if (e instanceof PrismaClientKnownRequestError) {
+			console.log({
+				code: e.code,
+				message: e.message,
+			});
+		} else if (e instanceof PrismaClientValidationError) {
+			console.log({
+				code: 400,
+				message: e.message,
+			});
+		} else {
+			console.error(e);
+			process.exit(1);
+		}
 	})
 	.finally(async () => {
 		await prisma.$disconnect();
+		process.exit(1);
 	});
