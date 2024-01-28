1364c5a4a8a7cc002f958ac3f6cb3a386fcd354f
Sufian_Majid <sufian.majid.999@gmail.com>
2024-01-28 21:38:13 +0500

    Create main object.

--- index.js
+++ index.js
@@ -2,11 +2,44 @@ const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 
-function main() {}
+main = {
+	//Create Driver
+	createDriver: async () => {
+		try {
+			return await prisma.driver.create({
+				data: {
+					email: 'something01@gmai.com',
+					name: 'radiohead',
+				},
+			});
+		} catch (e) {
+			console.error(e);
+			throw Error(e);
+		}
+	},
 
-main()
-	.then(() => console.log('Main ran successfully!'))
-	.catch((e) => console.log(e))
+	// Get driver
+	getDriver: async (id) => {
+		try {
+			return await prisma.driver.findUniqueOrThrow({ where: { id: id } });
+		} catch (e) {
+			if (e.code === 'P2025') {
+				console.log('Driver not found');
+			} else {
+				console.error(e);
+				throw Error(e);
+			}
+		}
+	},
+};
+main.createDriver()
+	.then((data) => {
+		console.log('Ran successfully!');
+		console.log(data);
+	})
+	.catch((e) => {
+		console.error(e);
+	})
 	.finally(async () => {
 		await prisma.$disconnect();
 	});
